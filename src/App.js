b1c04c3d1e18a7159c67ffff80548488483d6bde
Morad17 <morad123@hotmail.co.uk>
2024-10-13 18:20:43 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+import { Outlet,createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
+
+import "./assets/scss/base.scss"
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Footer from "./components/Footer"
 
 function App() {
+
+  const Layout = () => {
+    return (<div className="main-layout">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>)
+  }
+
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element: <Layout/>,
+      children: [{
+        path: "/",
+        element: 
+          <Home />
+      },
+    ]
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
